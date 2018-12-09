906628b416f0ded253dbce98411ac90d9c013006
Nic Waller <code@nicwaller.com>
2018-12-08 23:12:34 -0800

    day 9 part 2
    I should have used a deque from the beginning, but I mentally ruled it out too soon. Oops.

--- day09/solution.py
+++ day09/solution.py
@@ -1,79 +1,37 @@
-import networkx as nx
-import sys
 from collections import deque
 
-def load(filename):
-    edges = []
-    with open(filename) as f:
-        for line in f:
-            a = line[5:6]
-            b = line[36:37]
-            edges.append((a, b))
-    return edges
 
-
-# What is the winning Elf's score?
-def part1(players, marbles):
-    circle = []
+def play_game(players, marbles):
+    circle = deque()
     player_scores = [0] * players
-    current_player = 0
 
     # place the first marble
-    current_marble_place = 0
-    circle.insert(0, 0)
+    circle.append(0)
 
-    for current_marble_value in range(1, marbles+1):
-        # next player, ready your marble
-        current_player = (current_player + 1) % players
-
-        if current_marble_value % 23 == 0:
+    for marble_value in range(1, marbles+1):
+        if marble_value % 23 == 0:
             # score the marble
-            steal_location = (current_marble_place + len(circle) - 7) % len(circle)
-            # print(f"stealing from {steal_location}")
-            scored_points = current_marble_value + circle[steal_location]
-            # print(f"scored_points {scored_points}")
-            player_scores[current_player] += scored_points
-            circle.pop(steal_location)
-            current_marble_place = steal_location
+            circle.rotate(-7)
+            player_scores[marble_value % players] += marble_value + circle.popleft()
+            circle.rotate(1)
         else:
             # deposit next marble in the circle
-            current_marble_place = (current_marble_place + 2) % len(circle)
-            circle.insert(current_marble_place, current_marble_value)
-            # print(f"current_marble_place = {current_marble_place}")
-
-        if current_marble_value == marbles:
-            playing = False
-            break
-
-        # print(circle)
+            circle.rotate(1)  # clockwise = negative
+            circle.appendleft(marble_value)
 
     return max(player_scores)
 
 
-def part2(data):
-    return ''
-
-
-def check_value(expected=None, actual=None):
-    if expected == actual:
-        print('test passed')
-    else:
-        print(f'expected {expected} but got {actual}')
-
-
 def tests():
-    # part 1
-    check_value(32, part1(9, 25))
-    check_value(8317, part1(10, 1618))
-    check_value(146373, part1(13, 7999))
+    assert 32 == play_game(9, 25)
+    assert 8317 == play_game(10, 1618)
     print('Tests passed')
 
 
 def main():
     tests()
-    # 464 players; last marble is worth 70918 points
-    print('Part 1: ' + str(part1(464, 70918)))
-    # print('Part 2: ' + str(part2(data)))
+    print('Part 1: ' + str(play_game(464, 70918)))
+    print('Part 2: ' + str(play_game(464, 70918*100)))
 
 
 if __name__ == '__main__':
