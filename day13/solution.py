8ba59177d754abebfee8606f5c754b295a339834
Nic Waller <code@nicwaller.com>
2018-12-12 23:44:11 -0800

    day 13 part 2

--- day13/solution.py
+++ day13/solution.py
@@ -1,9 +1,10 @@
 import re
 from collections import defaultdict, deque
 from itertools import chain
-
+import copy
 
 collision = None
+g_bounds = (150, 150)
 
 
 def print_track(track, bounds, cars, highlight=(-1,-1)):
@@ -39,6 +40,7 @@ def load(filename):
                         'pos': (char_index, line_index),
                         'sprite': char,
                         'next_turn': 0,
+                        'destroyed': False,
                     })
                 elif char in "^v":
                     track[(char_index, line_index)] = '|'
@@ -46,6 +48,7 @@ def load(filename):
                         'pos': (char_index, line_index),
                         'sprite': char,
                         'next_turn': 0,
+                        'destroyed': False,
                     })
                 else:
                     track[(char_index, line_index)] = char
@@ -60,9 +63,10 @@ def turn(current, turn):
     return dirs[(dirs.index(current) + turn + 3) % 4]
 
 
-def advance_cars(track, all_cars):
+def advance_cars(track, all_cars, destroy=False):
     global collision
-    new_cars = list()
+    # new_cars = list()
+    new_cars = copy.deepcopy(sorted(all_cars, key=lambda c: c['pos'][1]*150 + c['pos'][0]))
 
     deltas = {
         '<': (-1,  0),
@@ -86,16 +90,29 @@ def advance_cars(track, all_cars):
         }
     }
     # carts on the top row move first
-    sorted_cars = sorted(all_cars, key=lambda c: c['pos'][1]*150 + c['pos'][0])
-    for index, car in enumerate(sorted_cars):
+    for index, car in enumerate(new_cars):
+        if car['destroyed'] == True:
+            continue
+
         pos = car['pos']
         motion = deltas[car['sprite']]
         new_pos = (pos[0] + motion[0], pos[1] + motion[1])
         new_tile = track[new_pos]
-        # TODO: check for collisions
-        if new_pos in (car['pos'] for car in chain(sorted_cars[index:len(sorted_cars)], new_cars[0:index])):
+
+        if new_pos in (car['pos'] for car in new_cars):
             collision = new_pos
-            raise Exception("Car Crash")
+            if destroy:
+                print('destroying car ' + str(car))
+                new_cars[index]['destroyed'] = True
+
+                second_car_index, second_car = next(filter(lambda c: c[1]['pos'] == new_pos, enumerate(new_cars)))
+                print('destroying second car: ' + str(second_car))
+                new_cars[second_car_index]['destroyed'] = True
+                # print_track(track, g_bounds, all_cars, highlight=new_pos)
+                continue
+            else:
+                collision = new_pos
+                raise Exception("Car Crash")
 
         new_sprite = car['sprite']
         next_turn = car['next_turn']
@@ -106,13 +123,14 @@ def advance_cars(track, all_cars):
         elif new_tile == '+':
             new_sprite = turn(new_sprite, car['next_turn'])
             next_turn = (next_turn + 1) % 3
-        new_cars.append({
+        new_cars[index] = {
             'pos': new_pos,
             'sprite': new_sprite,
             'next_turn': next_turn,
-        })
+            'destroyed': False,
+        }
 
-    return new_cars
+    return list(filter(lambda x: x['destroyed'] == False, new_cars))
 
 
 # What is the location of the first crash? X,Y
@@ -121,7 +139,7 @@ def part1(track, bounds, cars):
     prev_car_pos = cars
     # print_track(track, bounds, cars)
     for tick in range(1, 3000):
-        print(f"tick: {tick}")
+        # print(f"tick: {tick}")
         try:
             current_car_pos = advance_cars(track, prev_car_pos)
         except Exception as e:
@@ -132,6 +150,23 @@ def part1(track, bounds, cars):
         prev_car_pos = current_car_pos
 
     raise Exception('No crash occurred during test')
+
+# What is the location of the last cart at the end of the first tick where it is the only cart left?
+def part2(track, bounds, cars):
+    prev_car_pos = cars
+    # print_track(track, bounds, cars)
+    for tick in range(1, 50000):
+        if tick % 50 == 0:
+            print(f"tick: {tick}")
+            print(f"Remaining carts: {len(prev_car_pos)}")
+        current_car_pos = advance_cars(track, prev_car_pos, destroy=True)
+        if len(current_car_pos) == 1:
+            return current_car_pos[0]['pos']
+        # print_track(track, bounds, current_car_pos)
+        prev_car_pos = current_car_pos
+
+    raise Exception('No crash occurred during test')
+
 
 
 def tests():
@@ -148,12 +183,19 @@ def tests():
 def main():
     tests()
     track, bounds, cars = load('puzzle_input')
-    part1_guess = part1(track, bounds, cars)
-    print(part1_guess)
-    # Part 1 answer is not: 50,107
-    assert "50,107" != part1_guess
-    assert "116,91" == part1_guess
-    print("Part 1: " + str(part1_guess))
+
+    # part1_guess = part1(track, bounds, cars)
+    # print(part1_guess)
+    # # Part 1 answer is not: 50,107
+    # assert "50,107" != part1_guess
+    # assert "116,91" == part1_guess
+    # print("Part 1: " + str(part1_guess))
+
+    part2_guess = part2(track, bounds, cars)
+    print(part2_guess)
+    assert (130, 71) != part2_guess
+    assert (8, 23) == part2_guess
+    print("Part 2: " + str(part2_guess))
 
 
 if __name__ == '__main__':
