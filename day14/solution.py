7d77e78d495799d2ad16055dedbd4d543b5f10f8
Nic Waller <code@nicwaller.com>
2018-12-16 12:56:56 -0800

    day 14 - faster with Numba JIT

--- day14/solution.py
+++ day14/solution.py
@@ -1,29 +1,29 @@
 from __future__ import annotations
 from collections import deque
-from typing import Dict, Tuple, List, DefaultDict, Text, Set
+from typing import Dict, Tuple, List, DefaultDict, Text, Set, Iterable
 from itertools import islice
+from numba import jit
 
 
-def generate():
+# noinspection PyDefaultArgument
+@jit(nopython=True)
+def generate() -> Iterable[int]:
     scoreboard = [3, 7]
-    yield 0, '3'
-    yield 1, '7'
+    yield 3
+    yield 7
     elf1_index = 0
     elf2_index = 1
-    d = deque()
-    iterations = 2
+    stack = list()
     while True:
         new_recipe = scoreboard[elf1_index] + scoreboard[elf2_index]
         while new_recipe >= 10:
-            d.append(new_recipe % 10)
+            stack.append(new_recipe % 10)
             new_recipe //= 10
-        d.append(new_recipe % 10)
-        d.reverse()
-        scoreboard.extend(d)
-        for x in d:
-            yield iterations, str(x)
-            iterations += 1
-        d.clear()
+        stack.append(new_recipe % 10)
+        for x in range(len(stack)-1, -1, -1):
+            scoreboard.append(stack[x])
+            yield stack[x]
+        stack.clear()
 
         elf1_move = 1 + scoreboard[elf1_index]
         elf2_move = 1 + scoreboard[elf2_index]
@@ -35,32 +35,44 @@ def generate():
 def part1(puzzle_input: int) -> int:
     g = generate()
     [next(g) for _ in range(puzzle_input)]
-    return ''.join([t[1] for t in islice(g, 10)])
+    return ''.join([str(t) for t in islice(g, 10)])
 
 
 # How many recipes appear on the scoreboard to the left of the score sequence in your puzzle input?
-def part2(puzzle_input: Text, skip: int = 0) -> int:
-    pattern = list([c for c in puzzle_input])
+@jit(nopython=True)
+def part2_jit(pattern: List[int], skip: int = 0) -> int:
     g = generate()
     [next(g) for _ in range(skip)]
-    last_n = deque(maxlen=len(pattern))
+    counter = skip
     checkrange = range(len(pattern))
+
+    ring_size = len(pattern)
+    ring = [0] * ring_size
+    ring_index = 0
+
     while True:
-        last_n.append(next(g))
+        ring[ring_index] = next(g)
+        ring_index = (ring_index + 1) % ring_size
+        counter += 1
         # Check to see if last5 matches the pattern
         match = True
         for i in checkrange:
-            if pattern[i] != last_n[i][1]:
+            if pattern[i] != ring[(ring_index + i) % ring_size]:
                 match = False
                 break
         if match:
-            return last_n[0][0]
+            return counter - len(pattern)
+
+
+def part2(puzzle_input: Text, skip: int = 0) -> int:
+    pattern = list([int(c) for c in puzzle_input])
+    return part2_jit(pattern, skip=skip)
 
 
 def tests():
     # Generator
-    assert [(0, '3'), (1, '7'), (2, '1'), (3, '0')] == list(islice(generate(), 4))
-    assert '37101012451589167792' == ''.join([x[1] for x in islice(generate(), 20)])
+    assert [3, 7, 1, 0] == list(islice(generate(), 4))
+    assert '37101012451589167792' == ''.join([str(x) for x in islice(generate(), 20)])
     # Part 1
     assert '5158916779' == part1(9)
     # Part 2
